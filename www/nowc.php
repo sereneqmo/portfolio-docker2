4a3a43317453c1117ce0604861b24fbd9990f2f2
Student <student@10-19-112-3.dynapool.wireless.nyu.edu>
2019-08-15 13:28:59 -0400

    master

--- www/nowc.php
+++ www/nowc.php
@@ -1,5 +1,7 @@
 <body>
     <?php 
+    //cannot use the $_SERVER since rn we r in docker so for this we can only get the ip address
+    //of my docker container in this machine
 	$ip_server = file_get_contents("http://ipecho.net/plain");
 	echo "Server IP Address is: $ip_server";  
    ?>
